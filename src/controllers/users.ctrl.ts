bab205210880be62830bce61ce1963f90923d24a
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-06-25 06:13:30 -0300

    Added: Implementation of the getUserById method

--- src/controllers/users.ctrl.ts
+++ src/controllers/users.ctrl.ts
@@ -1,17 +1,26 @@
 import { Request, Response } from 'express';
-import { QueryResult } from 'pg'
+import { QueryResult, } from 'pg'
 import { ServiceResponse } from '../abstractions/ServiceResponse';
 import { HttpStatusCode as Http } from '../abstractions/HttpStatusCode';
 import { pool } from '../config/database';
 
-const getAllUser = async (_req: Request, res: Response) => {
-	const queryResponse: QueryResult = await pool.query("SELECT * FROM users");
-	const serviceResponse = new ServiceResponse(queryResponse.rows, true, "User list successfully obtained.", null);
-	return res.status(Http.OK).json(serviceResponse.JSON());
+const getAllUser = async (_req: Request, res: Response): Promise<Response> => {
+	try {
+		const queryResponse: QueryResult = await pool.query("SELECT * FROM users");
+		const serviceResponse = new ServiceResponse(queryResponse.rows, true, "User list successfully obtained.", null);
+		return res.status(Http.OK).json(serviceResponse.JSON());
+	} catch (error: any) {
+		console.error(error);
+		const errorServiceResponse = new ServiceResponse(null, true, "Something went wrong", "Internal server error");
+		return res.status(Http.INTERNAL_SERVER_ERROR).json(errorServiceResponse.JSON());
+	}
 };
 
-const getUserById = (_req: Request, res: Response) => {
-	return res.json({ msg: "user by id" });
+const getUserById = async (req: Request, res: Response): Promise<Response> => {
+	const queryResponse: QueryResult = await pool.query(`SELECT * FROM users WHERE id=${req.params.id}`);
+	const message = queryResponse.rowCount === 0 ? "User not found." : "User obtained successfully."
+	const serviceResponse = new ServiceResponse(queryResponse.rows, true, message, null);
+	return res.status(Http.OK).json(serviceResponse.JSON());
 };
 
 const createUser = (_req: Request, res: Response) => {
