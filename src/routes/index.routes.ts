e41a592d506acd3b19727751dd76b25aeb96d327
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-06-25 04:36:48 -0300

    Fix: endpoint put and delete

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get('/users', getAllUser);
 router.get('/users/:id', getUserById);
 router.post('/users', createUser);
-router.put('/users', updateUser);
-router.delete('/users', deleteUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
