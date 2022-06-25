0da44c57c90ddaa2fc45e9407f8aa68b08a0ce1f
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-06-25 05:10:57 -0300

    Added: getAllUsers controller implementation

--- src/controllers/users.ctrl.ts
+++ src/controllers/users.ctrl.ts
@@ -1,29 +1,35 @@
 import { Request, Response } from 'express';
+import { QueryResult } from 'pg'
+import { ServiceResponse } from '../abstractions/ServiceResponse';
+import { HttpStatusCode as Http } from '../abstractions/HttpStatusCode';
+import { pool } from '../config/database';
 
-const getAllUser = (_req: Request, res: Response) => {
-    return res.json({ msg: "all users" });
+const getAllUser = async (_req: Request, res: Response) => {
+	const queryResponse: QueryResult = await pool.query("SELECT * FROM users");
+	const serviceResponse = new ServiceResponse(queryResponse.rows, true, "User list successfully obtained.", null);
+	return res.status(Http.OK).json(serviceResponse.JSON());
 };
 
 const getUserById = (_req: Request, res: Response) => {
-    return res.json({ msg: "user by id" });
+	return res.json({ msg: "user by id" });
 };
 
 const createUser = (_req: Request, res: Response) => {
-    return res.json({ msg: "create user" });
+	return res.json({ msg: "create user" });
 };
 
 const updateUser = (_req: Request, res: Response) => {
-    return res.json({ msg: "update user" });
+	return res.json({ msg: "update user" });
 };
 
 const deleteUser = (_req: Request, res: Response) => {
-    return res.json({ msg: "delte user" });
+	return res.json({ msg: "delte user" });
 };
 
 export {
-    getAllUser,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser
+	getAllUser,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser
 }
